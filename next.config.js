fe7095eb38cc061b52b88c6e26a0c17d89994a30
Alex Barashkov <lnikell@gmail.com>
2023-03-23 22:00:50 +0100

    chore: add more todos

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     appDir: true,
   },
   // TODO: Add webpack rules for SVG optimization inline + url
+  // TODO: Not related to his file - Add husky, markdownlint, commitlint
+  // TODO: Check that autocomplete and props highlighting works properly for TypeScript in VSCode with the current configuration
 };
 
 module.exports = nextConfig;
