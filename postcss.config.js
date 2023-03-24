9d1adfe2b83d0e9f2b49269603134b00fccf7258
Sasha Sushko <sasha@sushko.me>
2023-03-24 17:18:19 +0200

    fix: remove useless TODO

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-// PostCSS configuration file is required by Tailwind CSS
-// See: https://tailwindcss.com/docs/guides/nextjs
 module.exports = {
-  plugins: [
-    // TODO: Do we need a postcss-import because it feels everything is working without it?
-    'postcss-import',
-    'tailwindcss/nesting',
-    'tailwindcss',
-    'autoprefixer',
-  ],
+  plugins: {
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
