a6dedaa7a180dd09e7c3c4211dc23dd4001ada2f
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-06-01 18:32:56 +0300

    feat: add logic for generating seo meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import getMetadata from '@/utils/get-metadata';
+
 const Home = () => {
-  const aaaa = "Hello World asdf asdf asdf asd fasd fasd fasd fasd fasdf asdf asd asdf asdf asd  asd fasdf";
-  return (
-    <main>
-      <div className="container my-4">Hello World</div><div className="container my-4">Hello World</div><div className="container my-4">Hello World</div>
-    </main>
-  );
+  return <div className="container">Hello World</div>;
 };
 
 export default Home;
+
+// FIXME: Add real data here
+export const metadata = getMetadata({
+  title: 'Home - Next.js TypeScript Tailwind Starter',
+  description: 'Next.js TypeScript Tailwind Starter',
+  pathname: '/',
+});
