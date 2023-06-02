1fe4fef95e630a2e491d0bfbc70c1afa990fcd0e
Sasha Sushko <sasha@sushko.me>
2023-06-02 17:08:04 +0300

    feat: update breakpoints initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,12 +32,12 @@ module.exports = {
     }),
     // FIXME: Check if the breakpoints ("screens") are correct for the project
     screens: {
-      '2xl': { max: '1919px' },
-      xl: { max: '1535px' },
+      xl: { max: '1439px' },
       lg: { max: '1279px' },
       md: { max: '1023px' },
       sm: { max: '767px' },
-      xs: { max: '359px' },
+      xs: { max: '639px' },
+      '2xs': { max: '413px' },
     },
   },
   corePlugins: {
